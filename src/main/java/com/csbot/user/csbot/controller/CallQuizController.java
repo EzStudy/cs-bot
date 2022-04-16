082327ef14107f6e0fc52f5061c36d9678672da8
go-coding1 <rhtjrwnszyzy@gmail.com>
2022-04-16 14:27:07 +0900

    CallQuizController.java 생성 및 업로드

---
+++ src/main/java/com/csbot/user/csbot/controller/CallQuizController.java
@@ -0,0 +1,17 @@
+package com.csbot.user.csbot.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CallQuizController {
+
+    @PostMapping("/CallQuiz")
+    public String callQuiz(HttpServletRequest req, HttpServletResponse resp){
+        System.out.println(req.getParameter("body"));
+        return "quiz";
+    }
+}
