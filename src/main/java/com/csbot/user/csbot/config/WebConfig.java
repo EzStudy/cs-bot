9024c8c6fe3db391838cde7d96cdabcfe017e5d5
go-coding1 <rhtjrwnszyzy@gmail.com>
2022-07-06 14:46:34 +0900

    cors 적용

--- src/main/java/com/csbot/user/csbot/config/WebConfig.java
+++ src/main/java/com/csbot/user/csbot/config/WebConfig.java
@@ -10,6 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:80");
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST");
+
+
     }
 }
